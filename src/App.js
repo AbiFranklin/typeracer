e9d5511cadfef15067c9f6b3644a427f665cfb4e
Abi <abifranklin@gmail.com>
2019-09-26 14:31:12 -0500

    Hok working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, {useState} from 'react';
 
 const App = () => {
   const [userText, setUserText] = useState('');
+
+  const updateUserText = e => {
+    setUserText(e.target.value);
+    console.log('Current User Text', userText);
+  }
+
   return (
     <div>
       <h2>Type Race</h2>
-      <input />
+      <input value={userText} onChange={updateUserText} />
     </div>
   )
 }
