29f29ab86d5d4b7bee7b63e3d68a9de4f72f1a7a
Abigayle Franklin <abifanklin@gmail.com>
2019-09-29 18:44:53 -0500

    Snippet buttons added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
 
 const App = () => {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const [snippet, setSnippet] = useState('');
   const [userText, setUserText] = useState('');
 
   const updateUserText = e => {
@@ -8,10 +14,25 @@ const App = () => {
     console.log('Current User Text', userText);
   }
 
+  const chooseSnippet = snippetIndex => () => {
+    console.log('set snippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+  };
+
   return (
     <div>
-      <h2>Type Race</h2>
+      <h2>Type Racer</h2>
+      <hr />
+      <h3>Snippet</h3>
+      {snippet}
       <input value={userText} onChange={updateUserText} />
+      <hr />
+      {SNIPPETS.map((SNIPPET, index) => (
+        // refactor SNIPPETS to include ids
+        <button onClick={chooseSnippet(index)} key={index}>
+        {SNIPPET.substring(0, 10)}...
+        </button>
+      ))}
     </div>
   )
 }
